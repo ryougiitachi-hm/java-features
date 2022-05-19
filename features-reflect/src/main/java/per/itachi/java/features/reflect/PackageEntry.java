447f256e1c2054f49803cc7c0862725ae30395f9
ryougiitachi <ryougiitachi@hotmail.com>
2022-05-19 21:13:13 +0800

    [features-reflect] Initialized module and content about package.

---
+++ features-reflect/src/main/java/per/itachi/java/features/reflect/PackageEntry.java
@@ -0,0 +1,9 @@
+package per.itachi.java.features.reflect;
+
+public class PackageEntry {
+
+    public static void main(String[] args) {
+        Package packet = Class.class.getPackage();
+//        packet.
+    }
+}
