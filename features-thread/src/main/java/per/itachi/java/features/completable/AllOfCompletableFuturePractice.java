3c4727e8df3d2922cbbc2736b20962f52cff68b0
Ryougi Itachi <ryougiitachi@hotmail.com>
2022-06-17 13:30:51 +0800

    [features-thread] New content about CompletableStage and CompletionService.

--- features-thread/src/main/java/per/itachi/java/features/completable/AllOfCompletableFuturePractice.java
+++ features-thread/src/main/java/per/itachi/java/features/completable/AllOfCompletableFuturePractice.java
@@ -2,6 +2,12 @@ package per.itachi.java.features.completable;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import lombok.extern.slf4j.Slf4j;
 import per.itachi.java.features.vo.AccountVO;
 import per.itachi.java.features.vo.OrderVO;
@@ -12,6 +18,9 @@ public class AllOfCompletableFuturePractice implements CompletableFuturePractice
 
     @Override
     public void practice() {
+        ExecutorService executorService = new ThreadPoolExecutor(
+                5, 5, 0,
+                TimeUnit.DAYS, new LinkedBlockingQueue<>());
         // initialize
         CompletableFuture<OrderVO> futurnOrder = CompletableFuture
                 .supplyAsync(()->{
@@ -19,21 +28,21 @@ public class AllOfCompletableFuturePractice implements CompletableFuturePractice
                     OrderVO orderVO = new OrderVO();
                     orderVO.setOrderNbr(UUID.randomUUID().toString());
                     return orderVO;
-                });
+                }, executorService);
         CompletableFuture<UserVO> futurnUser = CompletableFuture
                 .supplyAsync(()->{
                     log.info("Start handling user. ");
                     UserVO userVO = new UserVO();
                     userVO.setUsername(UUID.randomUUID().toString());
                     return userVO;
-                });
+                }, executorService);
         CompletableFuture<AccountVO> futurnAccount = CompletableFuture
                 .supplyAsync(()->{
                     log.info("Start handling account. ");
                     AccountVO accountVO = new AccountVO();
                     accountVO.setAccountNbr(UUID.randomUUID().toString());
                     return accountVO;
-                });
+                }, executorService);
 
         // executing
         log.info("Executing futures. ");
@@ -46,5 +55,7 @@ public class AllOfCompletableFuturePractice implements CompletableFuturePractice
         orderVO.setUsername(userVO.getUsername());
         orderVO.setPaidAccountNbr(accountVO.getAccountNbr());
         log.info("The result is {}. ", orderVO);
+
+        executorService.shutdown();
     }
 }
